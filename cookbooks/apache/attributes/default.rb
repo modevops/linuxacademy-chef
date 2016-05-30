322badcb0f4934c8e4ca02d6cef7a26d8cf52e42
root <root@mosborn1.mylabserver.com>
2016-05-30 18:02:16 +0000

    updating our workstation chef-repo

--- cookbooks/apache/attributes/default.rb
+++ cookbooks/apache/attributes/default.rb
@@ -2,6 +2,8 @@ default["apache"]["sites"]["mosborn2"] = {"site_title" => "Marcus Website Coming
 default["apache"]["sites"]["mosborn2b"] = {"site_title" => "Marcus2b Website Coming Soon", "port" => 80, "domain" => "mosborn2b.mylabserver.com"}
 default["apache"]["sites"]["mosborn3"] = {"site_title" => "Marcus3 Website Coming Soon", "port" => 80, "domain" => "mosborn3.mylabserver.com"}
 
+default["author"]["name"] = "Marcus"
+
 case node["platform"]
 when "centos"
 	default["apache"]["package"] = "httpd"
---
+++ environments/dev.rb
@@ -0,0 +1,4 @@
+name "dev"
+description "This is the development enviroment"
+cookbook "apache", "= 0.1.5"
+
